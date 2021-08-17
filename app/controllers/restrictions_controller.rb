fc6cc94b09c88382f225ce1ed6fa94d164b3e730
michellelally <shelley.lally@gmail.com>
2021-08-17 22:41:27 +0100

    restrictions updated

--- app/controllers/restrictions_controller.rb
+++ app/controllers/restrictions_controller.rb
@@ -1,5 +1,21 @@
 class RestrictionsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+
+    require 'net/http'
+    require 'json'
+
+    @url = "https://raw.githubusercontent.com/michellelally/json-resctrictions/main/restrictions.json"
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @output = JSON.parse(@response)
+
+    if @output.empty?
+      @final_output = "Error"
+    else 
+      @final_output = @output
+    end
   end 
 
 end
