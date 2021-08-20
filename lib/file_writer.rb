a77f6ca98d023b661303747b39ae47cba9b1bb72
michellelally <shelley.lally@gmail.com>
2021-08-20 17:01:12 +0100

    employees writing to file - singleton pattern working

--- lib/file_writer.rb
+++ lib/file_writer.rb
@@ -2,16 +2,16 @@ require 'singleton'
 class FileWriter
     include Singleton
     def initialize
-        @file_dir = "/employees.txt"
+        @file_dir = "lib/employees.txt"
         @log = File.open(@file_dir, "a")
     end
 
     def logInformation(information)
-        @file_dir = "/employees.txt"
+        @file_dir = "./employees.txt"
         @file = File.open(@file_dir, "a")
         @file.write(information)
 
-        File.open("/employees.txt", 'w') { |file| file.write(information) }
+        File.open("lib/employees.txt", 'w') { |file| file.write(information) }
 
     end
 end
